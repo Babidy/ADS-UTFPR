9b505b34d95c75d1d6f4c6704142ad89463527ac
David Prado <david.prado1@hotmail.com>
2020-03-05 20:05:17 -0300

    Inicio do codigo
    // Fatora um numero inteiro e retorna '1' para primo e '0' para não primo

---
+++ Funcoes/ehPrimo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int ehPrimo(int n){   // Fatora um numero inteiro e retorna '1' para primo e '0' para não primo
+  int resultado,
+      cont,
+      x;
+  
+  for(x=n; x>0; x--){
+  
+  
+  
+  }
+  
+
+
+  return resultado
+}
